a42731a0cd62747ac8af29d519bb6702241e6cb8
Johannes B <johannes.bock@live.de>
2020-04-24 15:22:32 +0200

    Projektdateien hinzufügen.

---
+++ KaffeemaschineOOP/Kaffeemaschine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_Kaffeemaschine
+{
+    class Kaffeemaschine
+    {
+        public double wasser { get; private set; }
+        public double bohnen { get; private set; }
+        public double gesamtMengeKaffeeProduziert { get; private set; }
+
+        private const double maxWasser = 2.5;
+
+        private const double maxBohnen = 2.5;
+
+        public Kaffeemaschine()
+        {
+            wasser = 0;
+            bohnen = 0;
+            gesamtMengeKaffeeProduziert = 0;
+        }
+
+        public double wasserAuffuellen(double menge)
+        {
+            if(menge <= 0)
+            {
+                return 0;
+            }
+
+            if(wasser + menge <= maxWasser)
+            {
+                wasser += menge;
+                return menge;
+            }
+
+            double tatsaechlicheMenge = maxWasser - wasser;
+            wasser = maxWasser;
+            return tatsaechlicheMenge;
+        }
+
+        public double bohnenAuffuellen(double menge)
+        {
+            if (menge <= 0)
+            {
+                return 0;
+            }
+
+            if (bohnen + menge <= maxBohnen)
+            {
+                bohnen += menge;
+                return menge;
+            }
+
+            double tatsaechlicheMenge = maxBohnen - bohnen;
+            bohnen = maxBohnen;
+            return tatsaechlicheMenge;
+        }
+
+        public bool macheKaffee(double menge, double verhaeltnisWasserBohnen)
+        {
+            if(menge <= 0 || verhaeltnisWasserBohnen <= 0)
+            {
+                return false;
+            }
+
+            double bohnenAnteil = menge / (verhaeltnisWasserBohnen + 1);
+            double wasserAnteil = menge - bohnenAnteil;
+
+            if(bohnenAnteil > bohnen || wasserAnteil > wasser)
+            {
+                return false;
+            }
+
+            bohnen -= bohnenAnteil;
+            wasser -= wasserAnteil;
+
+            gesamtMengeKaffeeProduziert += menge;
+            return true;
+        }
+    }
+}
---
+++ KaffeemaschineOOP/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OOP_Kaffeemaschine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaffeemaschine");
+            string t = "0";
+            Kaffeemaschine k = new Kaffeemaschine();
+            double menge0, verhaeltnis0;
+            while (t != "5")
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1: Kaffee machen");
+                Console.WriteLine("2: Wasser auffuellen");
+                Console.WriteLine("3: Bohnen auffuellen");
+                Console.WriteLine("4: Fuellstand abfragen");
+                Console.WriteLine("5: Beenden");
+                Console.WriteLine("Auswahl:");
+                t = Console.ReadLine();
+                switch (t)
+                {
+                    case "1":
+                        Console.WriteLine("Wieviel Kaffee soll gemacht werden?");
+                        menge0 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("In welchen Verhaeltnis?");
+                        verhaeltnis0 = double.Parse(Console.ReadLine());
+                        if (k.macheKaffee(menge0, verhaeltnis0))
+                        {
+                            Console.WriteLine("Die Maschine hat insgesamt " + k.gesamtMengeKaffeeProduziert + " kg Kaffee produziert.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nicht genug Zutaten, oder falsche Eingabe!");
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Wieviel Wasser soll aufgefuellt werden?");
+                        menge0 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Es wurde(n) " + k.wasserAuffuellen(menge0) + " l Wasser aufgefuellt.");
+                        break;
+                    case "3":
+                        Console.WriteLine("Wieviel Bohnen sollen aufgefuellt werden?");
+                        menge0 = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Es wurde(n) " + k.bohnenAuffuellen(menge0) + " kg Bohnen aufgefuellt.");
+                        break;
+                    case "4":
+                        Console.WriteLine("Fuellstand:");
+                        Console.WriteLine("Wasser: " + k.wasser.ToString("0.00") + " l");
+                        Console.WriteLine("Bohnen: " + k.bohnen.ToString("0.00") + " kg");
+                        Console.WriteLine("Kaffee: " + k.gesamtMengeKaffeeProduziert.ToString("0.00") + " kg");
+                        break;
+                    default:
+                        Console.WriteLine("Falsche Eingabe!");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+        
+    }
+
+
+}
